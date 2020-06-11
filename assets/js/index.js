244565bb29ea2085eeb59c39fd26a6d071a37a8e
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-06-11 00:03:49 -0500

    Nightly Commit - Navbar Semi-Finished, but bound for a redesign

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// NAVBAR SCROLLING CUSTOMIZATION
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-179px";
+  }
+  prevScrollpos = currentScrollPos;
+}
