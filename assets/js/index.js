a77aa59991c824912b217c317575dc7d32a2d908
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-06-13 03:19:17 -0500

    Final Commit for the day 6/13/20 || Added Google Maps API (WIP) Functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,13 +22,48 @@ function navbarMinimize() {
   }
 }
 
-// GOOGLE MAPS API
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+function changeStore() {
+  var selected = document.getElementById("stores").value;
+  console.log(selected)
+  
 }
+
+
+
+
+
+
+
+
+var map;
+var service;
+var infowindow;
+
+function initialize() {
+  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    query: 'restaurant'
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
+
+initialize()
