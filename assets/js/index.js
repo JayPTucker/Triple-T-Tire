d01c92d68395c50d590289b58d75f4562e2b702e
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-06-12 21:07:14 -0500

    Google Maps API Test

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,3 +21,14 @@ function navbarMinimize() {
     x.className = "header topnav sticky-top fixed-top container-fluid";
   }
 }
+
+// GOOGLE MAPS API
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
